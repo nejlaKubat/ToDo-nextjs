97c4b7bc62e0c338b25e55353a14216950d03eba
Nejla Kubat <nejla.kubat99@gmail.com>
2024-08-22 12:20:58 +0200

    Initial pages

---
+++ app/(dashboard)/todos/page.tsx
@@ -0,0 +1,10 @@
+export default function Todos() {
+    return (
+        <main className="flex min-h-screen flex-col items-center justify-between p-24">
+            <div>
+                Todos page
+            </div>
+        </main>
+        
+    )
+}
---
+++ app/docs/page.tsx
@@ -0,0 +1,9 @@
+export default function Docs() {
+    return (
+        <main className="flex min-h-screen flex-col items-center justify-between p-24">
+            <div>
+                Docs page
+            </div>
+        </main>
+    )
+}
