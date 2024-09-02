9a77f71b79a110c93e194e6ec81dea307b393177
Nejla Kubat <nejla.kubat99@gmail.com>
2024-09-02 14:27:17 +0200

    api

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 
-export default async function Home() {
+export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div>Home</div>
